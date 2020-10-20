9fcb4b0c32e0b4095ce6719a1376d3fa7c22ddde
Takahiro Hara <takahiro.hb@gmail.com>
2020-10-20 20:45:56 +0900

    add UserDefaults test code

--- PersistencePractice/ViewController.swift
+++ PersistencePractice/ViewController.swift
@@ -12,7 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Int
+        UserDefaults.standard.set(10, forKey: "Int")
+        print(UserDefaults.standard.integer(forKey: "Int"))
+        
+        // Dictionary
+        let dict: [String: Int] = ["first": 1, "second": 2, "third": 3]
+        UserDefaults.standard.set(dict, forKey: "dict")
+        
+        if let dict = UserDefaults.standard.dictionary(forKey: "dict") as? [String: Int] {
+            print(dict["first"]!)
+            print(type(of: dict["first"]!))
+        }
+        
+        // 用意されていない型
+        let date = Date()
+        UserDefaults.standard.setValue(date, forKeyPath: "date")
+        if let date = UserDefaults.standard.object(forKey: "date") as? Date {
+            print(date)
+        }
+        
+        // 削除
+        UserDefaults.standard.removeObject(forKey: "Int")
+        print(UserDefaults.standard.integer(forKey: "Int")) // 0
+        
+        UserDefaults.standard.removeObject(forKey: "dict")
+        print(UserDefaults.standard.dictionary(forKey: "dict")) // nil
     }
 
 
