5720a8cbbf0b20122bf9bc00160817986f84e7a1
Takahiro Hara <takahiro.hb@gmail.com>
2020-12-04 00:27:26 +0900

    use ANY, NONE, IN

--- RealmBasic/RealmBasic/ViewController.swift
+++ RealmBasic/RealmBasic/ViewController.swift
@@ -14,19 +14,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let realm = try! Realm()
+        let cat1 = Cat(value: ["name": "cat1", "age": 3])
+        let cat2 = Cat(value: ["name": "cat2", "age": 5])
+        let cat3 = Cat(value: ["name": "cat3", "age": 10])
+        
         try! realm.write {
             realm.deleteAll()
         }
         try! realm.write {
-            realm.add([Person(value: ["name": "A", "age": 20]),
-                       Person(value: ["name": "B", "age": 20, "countryCode": "jp"]),
-                       Person(value: ["name": "C", "age": 15, "countryCode": "jp"])])
+            realm.add([Person(value: ["name": "A", "age": 10, "cats": [cat1]]),
+                       Person(value: ["name": "B", "age": 20, "cats": [cat1, cat2]]),
+                       Person(value: ["name": "C", "age": 15, "cats": [cat1, cat2, cat3]])])
         }
-        
-        let code = "jp"
-        let age = 18
+
         var results = realm.objects(Person.self)
-        results = results.filter("age >= %@ && countryCode = %a", age, code)
-        print("results: \(results)")
+         
+        
+        print("results ANY: \(results.filter("ANY cats.age == 10"))")
+        print("results NONE: \(results.filter("NONE cats.age == 5"))")
+        print("results IN: \(results.filter("age IN {15, 20, 25}"))")
     }
 }
