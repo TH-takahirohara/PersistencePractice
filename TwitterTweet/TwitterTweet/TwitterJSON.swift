9a17c4d40ade389cf77cfbecc298c8a991253348
Takahiro Hara <takahiro.hb@gmail.com>
2021-01-04 00:36:50 +0900

    update TwitterJSON

--- TwitterTweet/TwitterTweet/TwitterJSON.swift
+++ TwitterTweet/TwitterTweet/TwitterJSON.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct TwitterJSON {
+    static let user: [String: Any] = [
+        "id": 12,
+        "name": "Jack Dorsey",
+        "screen_name": "jack",
+        "profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_0_normal.png"
+    ]
+}
