e59480450ed7dc791019c5f1f12363897e8e4ff5
Takahiro Hara <takahiro.hb@gmail.com>
2021-01-09 23:50:23 +0900

    add TweetCell

---
+++ TwitterTweet/TwitterTweet/TweetCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetCell.swift
+//  TwitterTweet
+//
+//  Created by 原昂大 on 2021/01/09.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
